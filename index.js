f2cd035c28914ea224c7743c94249ee840a0c043
Morinke01 <ejulius250@gmail.com>
2023-06-26 08:49:14 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+//Changing text on page when DOM has loaded
+const changeText = document.addEventListener("DOMContentLoaded", function() {
+    let a = document.getElementById("text")
+    a.textContent = "This is really cool!"
+
+})
+changeText();
